413a89c8f0159e18ec1354cee24b4302382150ad
HMasataka <siruba.2050@gmail.com>
2022-09-21 20:35:11 +0900

    Impl scoped insert

---
+++ scope.go
@@ -0,0 +1,90 @@
+package scope
+
+import (
+	"context"
+
+	"github.com/HMasataka/transactor"
+	"github.com/stretchr/testify/suite"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+)
+
+const contextScopeKey = "current_scope"
+
+type scope struct {
+	deletables []deletable
+}
+
+func withScope(ctx context.Context, s *scope) context.Context {
+	return context.WithValue(ctx, contextScopeKey, s)
+}
+
+func currentScope(ctx context.Context) *scope {
+	v := ctx.Value(contextScopeKey)
+	if v != nil {
+		return v.(*scope)
+	}
+	return nil
+}
+
+type insertable interface {
+	Insert(context.Context, boil.ContextExecutor, boil.Columns) error
+}
+
+type deletable interface {
+	Delete(ctx context.Context, exec boil.ContextExecutor) (int64, error)
+}
+
+type sqlboilerModel interface {
+	insertable
+	deletable
+}
+
+func NewClient(db transactor.ConnectionProvider, tx transactor.Transactor) *client {
+	return &client{
+		DB: db,
+		TX: tx,
+	}
+}
+
+type client struct {
+	suite.Suite
+	DB transactor.ConnectionProvider
+	TX transactor.Transactor
+}
+
+func (c *client) Scoped(ctx context.Context, fn func(ctx context.Context)) {
+	s := &scope{}
+	ctx = withScope(ctx, s)
+	defer func() {
+		if len(s.deletables) == 0 {
+			return
+		}
+		err := c.DeleteRows(ctx, s.deletables...)
+		c.Assert().NoError(err)
+	}()
+	fn(ctx)
+}
+
+func (c *client) Insert(ctx context.Context, i sqlboilerModel) {
+	conn := c.DB.CurrentConnection(ctx)
+
+	cs := currentScope(ctx)
+	cs.deletables = append(cs.deletables, i)
+
+	err := i.Insert(ctx, conn, boil.Infer())
+	c.Assert().NoError(err)
+}
+
+func (c *client) DeleteRows(ctx context.Context, deletables ...deletable) error {
+	conn := c.DB.CurrentConnection(ctx)
+
+	return c.TX.Required(ctx, func(ctx context.Context) error {
+		for _, deletable := range deletables {
+			_, err := deletable.Delete(ctx, conn)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+}
